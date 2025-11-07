975269014320f0e3f436d73fc14990d56e121a1b
Yousef Alzubaidi <yousef.z.a.z811@gmail.com>
2025-11-07 17:43:22 +0300

    CV Task

--- lib/main.dart
+++ lib/main.dart
@@ -11,61 +11,108 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        
+        appBar: AppBar( 
+        title: Text("CV"),
+        backgroundColor: Colors.teal,
+        ),
+        body: Column(
+          
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.blue,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 100,
+                    backgroundImage: Image.asset('images/1.jpg').image,
+              ) ,
+                  SizedBox(height: 20,),
+                    Text('yousef Alzubaidi'),
+                    Text('Developer')
+            ],
+            ),
+            ),
+          Divider(),
+          Container(
+          color: Colors.teal,
+          child: Column(
+
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                        Text('Name: Yousef'),
+                        Text('Email: yousef.z.z.z811@gamil.com')
+                        ],
+            ),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                        Text('PHone: 776713590'),
+                        Text('Sex: Male')
+                          ],
+                      )
+          ],
+          ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            color: Colors.teal,
+            child:Column(
+              children: [
+                Column(
+                  children: [
+                    Text('Skills'),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('1- Speaking English'),
+                                Text('2- Programming'),
+                                Text('3-using the computer very well'),
+                              ],
+
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Text('المؤهلات العلمية'),
+                    Row(
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('1-طالب تقنية معلومات سنة ثالث'),
+                                Text('2- حاصل على دبلوم متوسط في اللغة الانجليزية'),
+                              ],
+
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ],
+            ),
+            
+          )
+          
+          ],
+        ),)
+    );   
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,14 +123,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar( 
+      
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -103,20 +144,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),  // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-}
+
